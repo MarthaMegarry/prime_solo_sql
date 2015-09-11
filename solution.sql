bf5a1b30a4d93bb62fc79bd9b224057179bb3525
MarthaMegarry <mega0015@umn.edu>
2015-09-11 10:00:51 -0500

    got through question 8

--- solution.sql
+++ solution.sql
@@ -26,12 +26,33 @@ JOIN daily_inventory ON daily_inventory.isbn = editions.isbn
 WHERE daily_inventory.is_stocked = true;
 
 -- 7. Insert one of your favorite books into the database. Hint: You’ll want to create data in at least 2 other tables to completely create this book.
-
-
+--Found out you can't use double quotes for strings when inserting them into the database?? Also how did they get the arrays to work mine is now weird because the book itself isn't a string but it wouldn't let me do it any other way?? How do you string these together or do you just have to execute them seperately?
+INSERT INTO employees (id, last_name, first_name)
+VALUES (140, 'Megarry', 'Martha');
+INSERT INTO favorite_books (employee_id, books)
+VALUES (140, '{Cryptonomicon}');
+INSERT INTO authors (id, last_name, first_name)
+VALUES (456, 'Stephenson', 'Neil');
+INSERT INTO subjects (id, subject, location)
+VALUES (234, 'Cryptography', 'Philippines');
+INSERT INTO books (id, title, author_id, subject_id)
+VALUES (5678, 'Cryptonomicon', 456, 234);
 
 -- 8. What authors have books that are not in stock?
+SELECT authors.first_name, authors.last_name FROM authors 
+JOIN books ON books.author_id = authors.id
+JOIN editions ON editions.book_id = books.id
+JOIN daily_inventory ON daily_inventory.isbn = editions.isbn 
+WHERE daily_inventory.is_stocked = false;
+
 -- 9. What is the title of the book that has the most stock?
+--aggregate functions are not allowed in WHERE statements  
 
-
+--doodles/snippets on the theme:
+-- books.title FROM books 
+-- JOIN editions ON editions.book_id = books.id
+-- JOIN stock ON stock.isbn = editions.isbn
+-- SELECT * FROM stock ORDER BY stock DESC
+-- SELECT MAX(stock) FROM stock
 
 
